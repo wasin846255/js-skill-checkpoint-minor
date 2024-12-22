886ad3b19c0d69b7c3eb1c9270480fe8b99eaf95
Wasin <wasin1026@gmail.com>
2024-12-22 12:41:48 +0700

    Q2

--- question-2.js
+++ question-2.js
@@ -1,3 +1,21 @@
 // Question #2: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+
+async function fetchUser() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      
+      const newUsers = data.map(user => user.name);
+      console.log(newUsers);
+      
+      const longName = newUsers.filter(name => name.length > 17);
+      console.log(longName);
+    } catch (error) {
+      console.error('Failed to fetch users:', error);
+    }
+  }
+  
+  fetchUser();
+  
